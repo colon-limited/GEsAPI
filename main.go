a46375fd49e777922b0ba98a67771b8aa2d3341e
sys-cat <systemcat91@gmail.com>
2016-04-10 02:54:57 +0900

    add authorize methods.

--- main.go
+++ main.go
@@ -1,36 +1,81 @@
 package main
 
 import (
-  "gopkg.in/olivere/elastic.v3"
+  //"gopkg.in/olivere/elastic.v3"
   "github.com/BurntSushi/toml"
   "github.com/gin-gonic/gin"
+  // use for hash from scrypt
+  "encoding/base64"
+  "strings"
 )
 
+// Config
 type Config struct {
+  Base Base
   Auth Auth
-  ES ESConf
+  ES ES
 }
-
-type ESConf struct {
+type Base struct {
+  Port string `toml:"port"`
+}
+type ES struct {
   Url string `toml:"url"`
   Port string `toml:"port"`
 }
-
 type Auth struct {
   Salt string `toml:"salt"`
+  Pass string `toml:"seed"`
+}
+
+// Request Paramater
+type ReportList struct {
+  Auth string `json:"auth"`
+}
+type ReportDetail struct {
+  Auth string `json:"auth"`
+  Id int `json:"report"`
+}
+type SegmentList struct {
+  Auth string `json:"auth"`
+  Id int `json:"report"`
+}
+type SegmentUpdate struct {
+  Auth string `json:"auth"`
+  Id int `json:"report"`
+  Segment []SegmentItem `json:"segment"`
+}
+type SegmentItem struct {
+  Name string `json:"name"`
+  Cond string `json:"condition"`
 }
 
 var con Config
 
-func init {
+func init() {
   _, err := toml.DecodeFile("config.toml", &con)
   if err != nil {
     panic(err)
   }
 }
 
-func main {
-  route := gin.Default()
+/*
+* auth code is "base64encodedpass" + "," + "base64encodedsalt" strings over base64 encoded.
+*/
+func auth(st string)(res bool) {
+  de, _ := base64.StdEncoding.DecodeString(st)
+  spstr := strings.Split(string(de), ",")
+  pass_code, _ := base64.StdEncoding.DecodeString(spstr[0])
+  salt_code, _ := base64.StdEncoding.DecodeString(spstr[1])
+  if string(pass_code) == con.Auth.Pass && string(salt_code) == con.Auth.Salt {
+    res = true
+  } else {
+    res = false
+  }
+  return res
+}
+
+func main() {
+  router := gin.Default()
   v1 := router.Group("/v1")
   {
     v1.POST("/report", ListReports)
@@ -38,10 +83,42 @@ func main {
     v1.POST("/report/segment", GetSegment)
     v1.POST("/report/segment/edit", EditSegment)
   }
+  router.Run()
 }
 
-func authorization(key string) {}
-func ListReports(c *gin.Context) {}
-func GetReport(c *gin.Context) {}
-func GetSegment(c *gin.Context) {}
-func EditSegment(c *gin.Context) {}
+func ListReports(c *gin.Context) {
+  var req ReportList
+  c.BindJSON(&req)
+  if auth(req.Auth) {
+    c.JSON(200,gin.H{"status":"200",})
+  } else {
+    c.JSON(500,gin.H{"status":"500",})
+  }
+}
+func GetReport(c *gin.Context) {
+  var req ReportDetail
+  c.BindJSON(&req)
+  if auth(req.Auth) {
+    c.JSON(200,gin.H{"status":"200",})
+  } else {
+    c.JSON(500,gin.H{"status":"500",})
+  }
+}
+func GetSegment(c *gin.Context) {
+  var req SegmentList
+  c.BindJSON(&req)
+  if auth(req.Auth) {
+    c.JSON(200,gin.H{"status":"200",})
+  } else {
+    c.JSON(500,gin.H{"status":"500",})
+  }
+}
+func EditSegment(c *gin.Context) {
+  var req SegmentUpdate
+  c.BindJSON(&req)
+  if auth(req.Auth) {
+    c.JSON(200,gin.H{"status":"200",})
+  } else {
+    c.JSON(500,gin.H{"status":"500",})
+  }
+}
